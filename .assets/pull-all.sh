bc191f48e0b6e4578ca24fc84fe9e3fdf84f866d
Oleg Sucharevich <olegs@codefresh.io>
2019-06-25 09:50:44 +0300

    add daemon-set to pull images on new nodes

---
+++ .assets/pull-all.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+docker pull gcr.io/google-samples/microservices-demo/emailservice:v0.1.1
+docker pull gcr.io/google-samples/microservices-demo/checkoutservice:v0.1.1
+docker pull gcr.io/google-samples/microservices-demo/recommendationservice:v0.1.1
+docker pull gcr.io/google-samples/microservices-demo/frontend:v0.1.1
+docker pull gcr.io/google-samples/microservices-demo/paymentservice:v0.1.1
+docker pull gcr.io/google-samples/microservices-demo/productcatalogservice:v0.1.1
+docker pull gcr.io/google-samples/microservices-demo/cartservice:v0.1.1
+docker pull gcr.io/google-samples/microservices-demo/currencyservice:v0.1.1
+docker pull gcr.io/google-samples/microservices-demo/shippingservice:v0.1.1
+docker pull gcr.io/google-samples/microservices-demo/adservice:v0.1.1
+docker pull redis:alpine
